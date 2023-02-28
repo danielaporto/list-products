459b1f1ade384e65f0faf79164635e4dfacddd48
Daniela Aparecida Porto <danny.lorena@outlook.com>
2023-02-28 14:56:39 -0300

    feat: added clamp for text

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Container, ImagePost, TitlePost, DescriptionPost } from './styles';
+import Clamp from 'react-multiline-clamp';
 
 interface PostProps {
   posts: CardPostProps;
@@ -17,8 +18,12 @@ export const CardPost: FC<PostProps> = ({ posts }) => {
   return (
     <Container key={posts.id} className="CardPost">
       <ImagePost src={posts.cover} />
-      <TitlePost>{posts.title}</TitlePost>
-      <DescriptionPost>{posts.body}</DescriptionPost>
+      <Clamp lines={2}>
+        <TitlePost>{posts.title}</TitlePost>
+      </Clamp>
+      <Clamp lines={4}>
+        <DescriptionPost>{posts.body}</DescriptionPost>
+      </Clamp>
     </Container>
   );
 };
